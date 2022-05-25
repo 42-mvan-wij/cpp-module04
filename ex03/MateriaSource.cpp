526d4937888549ca1846f44db3727db175a3c964
Mariusmivw <mvan-wij@student.codam.nl>
2022-05-25 11:31:29 +0200

    Finish MateriaSource

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -12,19 +12,40 @@ MateriaSource::MateriaSource(const MateriaSource &src) {
 }
 
 MateriaSource &MateriaSource::operator=(MateriaSource const &rhs) {
-
+	for (int i = 0; i < NUM_MATERIAS; i++) {
+		if (this->materias[i] != nullptr)
+			delete this->materias[i];
+		if (rhs.materias[i] == nullptr)
+			this->materias[i] = nullptr;
+		else
+			this->materias[i] = rhs.materias[i]->clone();
+	}
 	return *this;
 }
 
 MateriaSource::~MateriaSource() {
-
+	for (int i = 0; i < NUM_MATERIAS; i++) {
+		if (this->materias[i] == nullptr)
+			continue ;
+		delete this->materias[i];
+	}
 }
 
 void MateriaSource::learnMateria(AMateria* materia) {
 	for (int i = 0; i < NUM_MATERIAS; i++) {
 		if (this->materias[i] == nullptr) {
-			this->materias[i] = materia->clone();
+			this->materias[i] = materia;
 			break ;
 		}
 	}
+}
+
+AMateria* MateriaSource::createMateria(std::string const& type) {
+	for (int i = 0; i < NUM_MATERIAS; i++) {
+		if (this->materias[i] == nullptr)
+			continue ;
+		if (this->materias[i]->getType() == type)
+			return this->materias[i]->clone();
+	}
+	return nullptr;
 }
